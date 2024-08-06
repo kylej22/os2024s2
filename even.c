a395fb33e520f07347f1144f8833e9e7bbc88958
Kyle <kylejohnston987654321@gmail.com>
2024-08-06 14:50:05 +0930

    even.c

--- even.c
+++ even.c
@@ -5,23 +5,26 @@
 
 void handle_sighup(int sig){
     printf("Ouch!\n");
+    fflush(stdout);
 }
 
 void handle_sigint(int sig){
     printf("Yeah!\n");
+    fflush(stdout);
 }
 
 int main(int argc, char*argv[]){
     int n = atoi(argv[1]);
 
-    signal(SIGHUP, handle_sighup);
-    signal(SIGINT, handle_sigint);
-
     // print first n even numbers
     for(int i = 1; i <= n; i++){
         printf("%d ", 2*i);
+        fflush(stdout);
+        signal(SIGHUP, handle_sighup);
+        signal(SIGINT, handle_sigint);
         sleep(5);
     }
 
     printf("\n");
+    return 0;
 }

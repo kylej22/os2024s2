1658e65edc127d5e2701c67cdd3de4f178b447af
Kyle <kylejohnston987654321@gmail.com>
2024-08-06 14:39:02 +0930

    even.c

--- even.c
+++ even.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void handle_sighup(int sig){
+    printf("Ouch!\n");
+}
+
+void handle_sigint(int sig){
+    printf("Yeah!\n");
+}
+
+int main(int argc, char*argv[]){
+    int n = atoi(argv[1]);
+
+    signal(SIGHUP, handle_sighup);
+    signal(SIGINT, handle_sigint);
+
+    // print first n even numbers
+    for(int i = 1; i <= n; i++){
+        printf("%d ", 2*i);
+        sleep(5);
+    }
+
+    printf("\n");
+}

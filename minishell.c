e67fb8f385e1c139dfa0d37e4ee205a124ae18ed
Kyle <kylejohnston987654321@gmail.com>
2024-08-07 13:28:53 +0930

    minishell.c first implementation

---
+++ minishell.c
@@ -0,0 +1,133 @@
+/*********************************************************************
+   Program  : miniShell                   Version    : 1.3
+ --------------------------------------------------------------------
+   skeleton code for linix/unix/minix command line interpreter
+ --------------------------------------------------------------------
+   File			: minishell.c
+   Compiler/System	: gcc/linux
+
+********************************************************************/
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#define NV 20  /* max number of command tokens */
+#define NL 100 /* input buffer size */
+char line[NL]; /* command input buffer */
+
+/*
+  shell prompt
+ */
+
+prompt(void)
+{
+  fprintf(stdout, "\n msh> ");
+  fflush(stdout);
+}
+
+main(int argk, char *argv[], char *envp[])
+/* argk - number of arguments */
+/* argv - argument vector from command line */
+/* envp - environment pointer */
+
+{
+  int frkRtnVal;       /* value returned by fork sys call */
+  int wpid;            /* value returned by wait */
+  char *v[NV];         /* array of pointers to command line tokens */
+  char *sep = " \t\n"; /* command line token separators    */
+  int i;               /* parse index */
+  int background;
+
+  /* prompt for and process one command line at a time  */
+
+  while (1)
+  { /* do Forever */
+    prompt();
+    fgets(line, NL, stdin);
+    fflush(stdin);
+
+    if (feof(stdin))
+    { /* non-zero on EOF  */
+
+      fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(),
+              feof(stdin), ferror(stdin));
+      exit(0);
+    }
+    if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
+      continue; /* to prompt */
+
+    v[0] = strtok(line, sep);
+    for (i = 1; i < NV; i++)
+    {
+      v[i] = strtok(NULL, sep);
+      if (v[i] == NULL)
+        break;
+    }
+
+    if (strcmp(v[0], "cd") == 0)
+    {
+      if (v[1] == NULL)
+      {
+        fprintf(stderr, "cd: missing argument\n");
+      }
+      else
+      {
+        if (chdir(v[1]) != 0)
+        {
+          perror("cd failed");
+        }
+      }
+      continue; /* to prompt */
+    }
+
+    background = 0;
+    if (i > 1 && strcmp(v[i - 1], "&") == 0)
+    {
+      background = 1;
+      v[i - 1] = NULL;
+    }
+
+    /* assert i is number of tokens + 1 */
+
+    /* fork a child process to exec the command in v[0] */
+
+    switch (frkRtnVal = fork())
+    {
+    case -1: /* fork returns error to parent process */
+    {
+      perror("Fork Failed");
+      break;
+    }
+    case 0: /* code executed only by child process */
+    {
+      // If exec fails, terminate the child process
+      if (execvp(v[0], v) == -1)
+      {
+        fprintf(stderr, "exec failed\n");
+        exit(1);
+      }
+    }
+    default: /* code executed only by parent process */
+    {
+      if(!background){
+        wpid = wait(0);
+        if(wpid == -1){
+          perror("wait failed");
+      } else {
+        printf("%s done\n", v[0]);
+      }
+      }
+      else {
+        printf("background process %d\n", frkRtnVal);
+      }
+      printf("%s done \n", v[0]);
+      break;
+    }
+    } /* switch */
+  } /* while */
+} /* main */
